21e86cc372ce10b248291a60fecc2f6028569cc2
NandhiniSK <51111386+NandhiniSK@users.noreply.github.com>
2023-03-15 11:56:44 +0530

    6 files modified

--- src/app/customer-list/customer/customer.component.ts
+++ src/app/customer-list/customer/customer.component.ts
@@ -1,13 +1,12 @@
-import { KeyValue } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Customer, CustomerDetails } from 'src/app/shared/model/customer.model';
-import { CustomerService } from 'src/app/shared/service/customer-service';
+import { Customer, CustomerDetails } from '../../shared/model/customer.model';
+import { CustomerService } from '../../shared/service/customer-service';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  styleUrls: ['./customer.component.css'],
 })
 export class CustomerComponent implements OnInit {
   taskList: any = {};
@@ -15,39 +14,40 @@ export class CustomerComponent implements OnInit {
   customerDetails: CustomerDetails = {} as CustomerDetails;
   isFeedbackOpen: boolean = false;
   isSevicePersonFeedbackOpen: boolean = false;
-  constructor(private route:ActivatedRoute, private router:Router, private customerService: CustomerService) { 
-    this.route.params.subscribe(param =>{
-      if(param["id"]){
-        this.customerID = +param["id"];
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private customerService: CustomerService
+  ) {
+    this.route.params.subscribe((param) => {
+      if (param['id']) {
+        this.customerID = +param['id'];
         this.getCustomer(this.customerID);
       }
       this.getTaskList();
     });
   }
 
-  ngOnInit() {
-    
-  }
+  ngOnInit() {}
 
-  getCustomer(customerID: number){
-    this.customerService.getCustomers()
-    .subscribe((response : Customer[]) => {
-      if(response){
-        this.customerDetails = response.find(x=> x.id == customerID) as CustomerDetails;
-        if(!this.customerDetails){
-          this.router.navigate(['/error']); 
+  getCustomer(customerID: number) {
+    this.customerService.getCustomers().subscribe((response: Customer[]) => {
+      if (response) {
+        this.customerDetails = response.find(
+          (x) => x.id == customerID
+        ) as CustomerDetails;
+        if (!this.customerDetails) {
+          this.router.navigate(['/error']);
         }
-      }
-      else 
-        this.router.navigate(['error']); 
+      } else this.router.navigate(['error']);
     });
   }
 
-  getTaskList(){
-    this.taskList["total count"] = 30;
-    this.taskList["pending"] = 2;
-    this.taskList["confirmed"] = 2;
-    this.taskList["On the way"] = 5;
-    this.taskList["completed"] = 21;
+  getTaskList() {
+    this.taskList['total count'] = 30;
+    this.taskList['pending'] = 2;
+    this.taskList['confirmed'] = 2;
+    this.taskList['On the way'] = 5;
+    this.taskList['completed'] = 21;
   }
 }
--- src/app/home/job-table/job-table.component.ts
+++ src/app/home/job-table/job-table.component.ts
@@ -1,58 +1,69 @@
 import { ThisReceiver } from '@angular/compiler';
 import { Component, Input, OnInit } from '@angular/core';
-import { Job } from 'src/app/shared/model/job.model';
-import { JobPipe } from 'src/app/shared/pipes/job-filter.pipe';
+import { Job } from '../../shared/model/job.model';
+import { JobPipe } from '../../shared/pipes/job-filter.pipe';
 
 @Component({
   selector: 'app-job-table',
   templateUrl: './job-table.component.html',
   styleUrls: ['./job-table.component.css'],
-  providers: [JobPipe]
+  providers: [JobPipe],
 })
 export class JobTableComponent implements OnInit {
-
   sortable: sortable;
-  columnList:any[] = [];
+  columnList: any[] = [];
   @Input() jobList: Job[] = [];
-  @Input() filterVal: string = "";
-  @Input() filterType: string = "";
+  @Input() filterVal: string = '';
+  @Input() filterType: string = '';
   filteredList: Job[] = [];
   rowCount: number = 5;
-  rowCountList: number[] = [5,10,15];
+  rowCountList: number[] = [5, 10, 15];
   p: number = 1;
-  constructor(private jobFilterPipe : JobPipe) { 
+  constructor(private jobFilterPipe: JobPipe) {
     this.getColumns();
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  getColumns(){
+  getColumns() {
     this.columnList = [
-      {label:"Job ID", key: "job_id", sortable:true},
-      {label: "Status", key:"status", sortable:true},
-      {label: "Assigned To", key:"assigned_to", sortable:true},
-      {label: "Location", key:"location", sortable:true}
+      { label: 'Job ID', key: 'job_id', sortable: true },
+      { label: 'Status', key: 'status', sortable: true },
+      { label: 'Assigned To', key: 'assigned_to', sortable: true },
+      { label: 'Location', key: 'location', sortable: true },
     ];
-    this.sortable = {columnName:this.columnList[0].key, isAscending:true};
+    this.sortable = { columnName: this.columnList[0].key, isAscending: true };
   }
 
-  ngOnChanges(){
-    let filterList = this.jobFilterPipe.transform(this.jobList, this.filterType, this.filterVal);
+  ngOnChanges() {
+    let filterList = this.jobFilterPipe.transform(
+      this.jobList,
+      this.filterType,
+      this.filterVal
+    );
     this.onFilterChange(filterList);
   }
 
-  
-  onFilterChange(filterList){
-    if(this.sortable.isAscending)
-      this.filteredList = filterList.sort((a, b) => a[this.sortable.columnName] < b[this.sortable.columnName] ? -1 : a[this.sortable.columnName] > b[this.sortable.columnName] ? 1 : 0)
+  onFilterChange(filterList) {
+    if (this.sortable.isAscending)
+      this.filteredList = filterList.sort((a, b) =>
+        a[this.sortable.columnName] < b[this.sortable.columnName]
+          ? -1
+          : a[this.sortable.columnName] > b[this.sortable.columnName]
+          ? 1
+          : 0
+      );
     else
-      this.filteredList = filterList.sort((a, b) => a[this.sortable.columnName] > b[this.sortable.columnName] ? -1 : a[this.sortable.columnName] < b[this.sortable.columnName] ? 1 : 0)
-    
-      
+      this.filteredList = filterList.sort((a, b) =>
+        a[this.sortable.columnName] > b[this.sortable.columnName]
+          ? -1
+          : a[this.sortable.columnName] < b[this.sortable.columnName]
+          ? 1
+          : 0
+      );
   }
 
-  OnSortClicked(key, isSameKey){
+  OnSortClicked(key, isSameKey) {
     this.sortable.columnName = key;
     this.sortable.isAscending = isSameKey ? !this.sortable.isAscending : true;
     this.onFilterChange(this.filteredList);
@@ -60,7 +71,7 @@ export class JobTableComponent implements OnInit {
   }
 }
 
-class sortable{
+class sortable {
   columnName: string;
   isAscending: boolean;
 }
